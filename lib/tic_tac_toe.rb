e7e4830163465248773922f5e608d9a98084d194
Jessica Rowland <rowlandjl@vcu.edu>
2019-09-24 13:15:45 +0000

    Done.

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,130 @@
+class TicTacToe
+
+  def initialize
+    @board = Array.new(9, " ")
+  end
+
+  WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ]
+
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
+  def input_to_index(input)
+    input.to_i - 1
+  end
+
+  def move(index, token = "X")
+    @board[index] = token
+  end
+
+  def position_taken?(index)
+    !(@board[index].nil? || @board[index] == " ")
+  end
+
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(index)
+  end
+
+  def turn
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+
+    if valid_move?(index)
+      move(index, current_player)
+      display_board
+    else
+      turn
+    end
+
+  end
+
+  def turn_count
+    turn = 0
+
+    @board.each do |token|
+      if token == "X" || token == "O"
+        turn += 1
+      else
+        turn += 0
+      end
+    end
+
+    turn
+  end
+
+  def current_player
+    turn_count.even? ? "X" : "O"
+  end
+
+  def won?
+    WIN_COMBINATIONS.each_with_index do |element, index|
+      if position_taken?(element[0]) == true 
+        if @board[element[0]] == @board[element[1]] && @board[element[1]] == @board[element[2]]
+          return WIN_COMBINATIONS[index]
+        end
+      end
+    end
+    return false
+  end
+
+  def full?
+    @board.all? do |token|
+      token == "X" || token == "O"
+    end
+  end
+
+  def draw?
+    !won? && full?
+  end
+
+  def over?
+    won? || draw? || full?
+  end
+
+  def winner
+    win = won?
+
+    if win
+      @board[win[0]]
+    else
+      nil
+    end
+    # if !won?
+    #   nil
+    # else
+    #   win = won?
+    #   win.each do |index|
+    #     win = @board[index]
+    #   end
+    #   win
+    # end
+  end
+
+  def play
+    until over?
+      turn
+    end
+
+    if won?
+      puts "Congratulations #{winner}!"
+    elsif draw?
+      puts "Cat's Game!"
+    end
+  end
+
+end
